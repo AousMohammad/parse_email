c559091c418ffed55efd863049e2b877d3d80850
Aous Mohammad <65487317+AousMohammad@users.noreply.github.com>
2023-10-17 09:12:25 +0300

    trying latest data

--- parse_email_server.py
+++ parse_email_server.py
@@ -5,6 +5,7 @@ from decouple import config
 import spacy
 import re
 import logging
+import en_core_web_sm
 
 logging.basicConfig(filename='email_processing.log', level=logging.INFO)
 
@@ -18,16 +19,18 @@ DB_PASS = config('DB_PASS')
 DB_NAME = config('DB_NAME')
 
 
-nlp = spacy.blank("en")
-ner = nlp.add_pipe("ner")
-labels = ["NAME", "COMPANY", "PHONE", "BUDGET", "COUNTRY"]
+# nlp = spacy.blank("en")
+# ner = nlp.add_pipe("ner")
+# labels = ["NAME", "COMPANY", "PHONE", "BUDGET", "COUNTRY"]
 
-for label in labels:
-    ner.add_label(label)
+# for label in labels:
+#     ner.add_label(label)
 
-# Load your custom NER model
-nlp.from_disk("astudio_email_parser")
-nlp.initialize()
+# # Load your custom NER model
+# nlp.from_disk("astudio_email_parser")
+# nlp.initialize()
+
+nlp = en_core_web_sm.load()
 
 def fetch_emails():
     mail = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
@@ -78,16 +81,27 @@ def extract_email_info(email_text):
     }
 
     for ent in doc.ents:
-        if ent.label_ == 'PHONE':
-            info['phone'] = ent.text
-        elif ent.label_ == 'COMPANY':
-            info['company'] = ent.text
-        elif ent.label_ == 'COUNTRY':
-            info['country'] = ent.text
-        elif ent.label_ == 'BUDGET':
+        if ent.label_ == 'CARDINAL' and "k" in ent.text.lower():  # This is a simple heuristic for budgets
             info['budget'] = ent.text
-        elif ent.label_ == 'NAME':
+        elif ent.label_ == 'PERSON' and not info['name']:
             info['name'] = ent.text
+        elif ent.label_ == 'ORG' and not info['company']:
+            info['company'] = ent.text
+        elif ent.label_ == 'GPE' and not info['country']:
+            info['country'] = ent.text
+
+    # Using regex to capture phone numbers
+    phone_pattern = re.compile(r'\+\d{1,3} \d{7,10}')
+  # Adjust regex as needed
+    phone_match = phone_pattern.search(email_text)
+    if phone_match:
+        info['phone'] = phone_match.group(0)
+
+    # Using regex to capture budget in the form of "30k-50k"
+    budget_pattern = re.compile(r'\d+k-\d+k')
+    budget_match = budget_pattern.search(email_text)
+    if budget_match:
+        info['budget'] = budget_match.group(0)
 
     return info
 
@@ -116,8 +130,9 @@ def store_to_db(data):
 
 def main():
     for email_data in fetch_emails():
-        print(email_data)
-        data = extract_email_info(email_data['content'])
+        clean_data = email_data['content'].replace('\r\n', ' ')
+        print(clean_data)
+        data = extract_email_info(clean_data)
         data['sender'] = email_data['sender']
         data['subject'] = email_data['subject']
         print(data)
